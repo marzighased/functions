16e70a3e7f4c465d7baaa617cddd67cd80ca113c
Marzi Ghasedian <marzighased@gmail.com>
2025-08-24 18:51:26 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//foreach
+/*
+array.foreach(function(currentValue, index, array) {
+codes
+})
+*/
+let persons = ["ali", "reza", "amir", "arash"];
+
+persons.forEach(function (person) {
+  console.log(`Hello ${person}`);
+});
