e8206f5c70320f8706d437a8c981501790df2017
Marzi Ghasedian <marzighased@gmail.com>
2025-08-27 21:43:36 +0200

    second map methode exercise

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ let persons = ["ali", "reza", "amir", "arash"];
 });
 console.log(numbers);*/
 
-let users = [
+/*let users = [
     { id: 1, name: "sara", email: "sara@gmail.com" },
     { id: 2, name: "tina", email: "tina@gmail.com" },
     { id: 3, name: "mary", email: "mary@gmail.com" },
@@ -45,5 +45,17 @@ let numbers = [1, 2, 3, 4];
 let doubleNumber = numbers.map(function(number) {
     return number*2
 })
-console.log('doubleNumber', doubleNumber)
-console.log('numbers', numbers)
+//console.log('doubleNumber', doubleNumber)
+//console.log('numbers', numbers)
+
+let users = [
+    { id: 1, name: "sara", email: "sara@gmail.com" },
+    { id: 2, name: "tina", email: "tina@gmail.com" },
+    { id: 3, name: "mary", email: "mary@gmail.com" },
+    { id: 4, name: "mina", email: "mina@gmail.com" }
+];
+
+let userNames = users.map(function(user) {
+    return user.name
+})
+console.log(userNames)
